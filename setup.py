8f5efa23167a7752eb0895d049c17fb1efa20509
Alexander X <alexfomalhaut@gmail.com>
2025-11-10 10:11:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
 from setuptools import setup
 
-from pcbscript import __version__
+
+with open('README.md') as f:
+    long_description = f.read()
 
 
 setup(
     name='pcbscript',
-    version=__version__,
+    version="1.1.0",
     description='A library to design PCB layout using an internal DSL.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Alexander Khlebushchev',
-    author_email='mail@fomalhaut.su',
-    packages=[
-        'pcbscript',
-    ],
+    author_email='alexfomalhaut@gmail.com',
+    url="https://github.com/fomalhaut88/pcbscript",
+    project_urls={
+        "Homepage": "https://github.com/fomalhaut88/pcbscript",
+        "Source": "https://github.com/fomalhaut88/pcbscript",
+        "Issues": "https://github.com/fomalhaut88/pcbscript/issues",
+    },
+    license="MIT",
+    packages=['pcbscript'],
     entry_points = {
         'console_scripts': ['pcbscript = pcbscript:main']
     },
     zip_safe=False,
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=[
+        'CairoSVG>=2.8',
+        'Pillow>=12',
+        'svgwrite>=1.4',
+    ],
+    python_requires=">=3.12",
+    keywords="pcb",
 )
